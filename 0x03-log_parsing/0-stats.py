8ce1b0b07bf183a99c4857ba5ce7708f09bb8d17
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-25 19:45:47 +0300

    :white_check_mark:Complete:Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,10 +4,11 @@
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
+def parse_log_line(log_line):
+    '''Parses a line of an HTTP request log and extracts relevant information.
+
     Args:
-        input_line (str): A line from the HTTP request log.
+        log_line (str): A line from the HTTP request log.
 
     Returns:
         dict: A dictionary containing status code and file size.
@@ -20,8 +21,8 @@ def extract_input(input_line):
         r'\s*(?P<file_size>\d+)'
     )
     log_fmt = '{}\\-{}{}{}{}\\s*'.format(pattern[0], pattern[1], pattern[2],
-                                        pattern[3], pattern[4])
-    match = re.fullmatch(log_fmt, input_line)
+                                         pattern[3], pattern[4])
+    match = re.fullmatch(log_fmt, log_line)
     info = {
         'status_code': 0,
         'file_size': 0,
@@ -31,3 +32,63 @@ def extract_input(input_line):
         info['file_size'] = int(match.group('file_size'))
     return info
 
+
+def update_metrics(log_line, current_total_size, status_code_counts):
+    '''Updates the metrics based on a given HTTP request log line.
+
+    Args:
+        log_line (str): A line from the HTTP request log.
+        current_total_size (int): The current total file size.
+        status_code_counts (dict): Dictionary counts for each status code.
+
+    Returns:
+        int: The new total file size.
+    '''
+    log_info = parse_log_line(log_line)
+    status_code = log_info.get('status_code', '0')
+    if status_code in status_code_counts:
+        status_code_counts[status_code] += 1
+    new_total_size = current_total_size + log_info['file_size']
+    return new_total_size
+
+
+def print_statistics(total_file_size, status_codes_counts):
+    '''Prints the accumulated statistics of the HTTP request log.
+
+    Args:
+        total_file_size (int): The total file size.
+        status_code_counts (dict): Dictionary counts for each status code.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_counts.keys()):
+        num = status_codes_counts.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+if __name__ == "__main__":
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
