dcb1e595b3ab2cbba1b6f9f42dbc2d7feebee3d4
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-01 12:45:27 +0300

    :white_check_mark:Utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,6 @@ def validUTF8(data):
     """Checks if a list of integers represents valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    # Initialize variables
     remaining_bytes = 0
     data_length = len(data)
 
@@ -19,50 +18,49 @@ def validUTF8(data):
             continue
 
         # Validate the current integer
-        current_integer = data[index]
-        if (type(current_integer) != int or current_integer < 0
-           or current_integer > 0x10ffff):
+        curr_int = data[index]
+        if type(curr_int) != int or curr_int < 0 or curr_int > 0x10ffff:
             return False
-        elif current_integer <= 0x7f:
+        elif curr_int <= 0x7f:
             remaining_bytes = 0
-        elif current_integer & 0b11111000 == 0b11110000:
+        elif curr_int & 0b11111000 == 0b11110000:
             # 4-byte UTF-8 character encoding
             expected_bytes = 4
             if data_length - index >= expected_bytes:
                 # Check the format of subsequent bytes
-                valid_body = all(map(
+                valid_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[index + 1: index + expected_bytes],
-                ))
-                if not valid_body:
+                    data[index + 1: index + expected_bytes]
+                    ))
+                if not all(valid_body):
                     return False
                 remaining_bytes = expected_bytes - 1
             else:
                 return False
-        elif current_integer & 0b11110000 == 0b11100000:
+        elif curr_int & 0b11110000 == 0b11100000:
             # 3-byte UTF-8 character encoding
             expected_bytes = 3
             if data_length - index >= expected_bytes:
                 # Check the format of subsequent bytes
-                valid_body = all(map(
+                valid_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[index + 1: index + expected_bytes],
-                ))
-                if not valid_body:
+                    data[index + 1: index + expected_bytes]
+                    ))
+                if not all(valid_body):
                     return False
                 remaining_bytes = expected_bytes - 1
             else:
                 return False
-        elif current_integer & 0b11100000 == 0b11000000:
+        elif curr_int & 0b11100000 == 0b11000000:
             # 2-byte UTF-8 character encoding
             expected_bytes = 2
             if data_length - index >= expected_bytes:
                 # Check the format of subsequent bytes
-                valid_body = all(map(
+                valid_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[index + 1: index + expected_bytes],
-                ))
-                if not valid_body:
+                    data[index + 1: index + expected_bytes]
+                    ))
+                if not all(valid_body):
                     return False
                 remaining_bytes = expected_bytes - 1
             else:
