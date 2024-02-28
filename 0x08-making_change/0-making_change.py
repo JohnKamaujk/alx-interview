1ef8e768c951eec28b379555f5a112ac2e5d2555
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-28 17:43:47 +0300

    :memo:Docs:Readme update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""Making change module.
+"""
+Making change module.
 """
 
 
 def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
+    amount total when given a pile of coins of different values,
+    using a greedy approach.
+    Args:
+        coins (list of int): The denominations of coins available.
+        total (int): The total amount to make change for.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total amount.
+            Returns -1 if the total cannot be met by any combination of coins.
     """
     if total <= 0:
         return 0
-    # list to store the fewest coins needed to make each amount from 0 to total
-    min_coins_needed = [float('inf')] * (total + 1)
-    min_coins_needed[0] = 0  # Base case: 0 coins needed to make amount 0
 
-    for coin in coins:
-        # Update min_coins_needed[i] if using this coin reduces the number
-        # of coins needed to make amount i
-        for i in range(coin, total + 1):
-            min_coins_needed[i] = min(min_coins_needed[i],
-                                      min_coins_needed[i - coin] + 1)
+    coins.sort(reverse=True)  # Sort coins in descending order
 
-    # If min_coins_needed[total] is still float('inf'),total
-    # cannot be met by any combination of coins
-    if min_coins_needed[total] != float('inf'):
-        return min_coins_needed[total]
+    coins_count = 0
+    idx = 0
+
+    while total > 0 and idx < len(coins):
+        if coins[idx] <= total:
+            total -= coins[idx]
+            coins_count += 1
+        else:
+            idx += 1
+
+    if total == 0:
+        return coins_count
     else:
         return -1
