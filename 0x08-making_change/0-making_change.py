d3a484180d3b4b02d66a1dc2e007cf984197c1fb
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-28 12:39:42 +0300

    :white_check_mark:Make change challenge

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Making change module.
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    # list to store the fewest coins needed to make each amount from 0 to total
+    min_coins_needed = [float('inf')] * (total + 1)
+    min_coins_needed[0] = 0  # Base case: 0 coins needed to make amount 0
+
+    for coin in coins:
+        # Update min_coins_needed[i] if using this coin reduces the number
+        # of coins needed to make amount i
+        for i in range(coin, total + 1):
+            min_coins_needed[i] = min(min_coins_needed[i],
+                                      min_coins_needed[i - coin] + 1)
+
+    # If min_coins_needed[total] is still float('inf'), total
+    # cannot be met by any combination of coins
+    if min_coins_needed[total] != float('inf'):
+        return min_coins_needed[total]
+    else:
+        return -1
